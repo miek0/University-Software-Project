7ac5bd52405522331361b4fe706d1b4267f6bcc7
Kohvu <44853020+Kohvu@users.noreply.github.com>
2019-05-01 13:09:27 -0700

    professor and course update

--- UniversitySoftware/src/Professor.java
+++ UniversitySoftware/src/Professor.java
@@ -1,4 +1,20 @@
+import java.util.ArrayList;
+
+import universitysoftware.Course;
 
 public class Professor {
-
+	protected ArrayList<Course> courseTaught;
+	
+	public Professor(){
+		
+	}
+	
+	void addCourse(Course course){
+		courseTaught.add(course);
+	}
+	
+	void removeCourse(int index) {
+		courseTaught.remove(index);
+	}
+	
 }
