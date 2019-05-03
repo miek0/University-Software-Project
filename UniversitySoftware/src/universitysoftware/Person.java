2d814e2a75f39cac83d781594d0aa9b1c29da58d
Kohvu <44853020+Kohvu@users.noreply.github.com>
2019-05-02 17:15:33 -0700

    Add files via upload

---
+++ UniversitySoftware/src/universitysoftware/Person.java
@@ -0,0 +1,36 @@
+package universitysoftware;
+import java.util.ArrayList;
+
+public class Person {
+	private String fName;
+	private String lName;
+	private String mName;
+	
+	public Person() {
+
+	}
+	
+	void setfName(String name){
+		fName = name;
+	}
+	
+	String getfName() {
+		return fName;
+	}
+	
+	void setlName(String name){
+		fName = name;
+	}
+	
+	String getlName() {
+		return fName;
+	}
+	
+	void setmName(String name){
+		fName = name;
+	}
+	
+	String getmName() {
+		return fName;
+	}	
+}
