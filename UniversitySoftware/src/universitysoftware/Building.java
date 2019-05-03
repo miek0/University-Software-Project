647b864ad4efa208573dd81c81fadd828c1f16c3
Mike <meikzeng@gmail.com>
2019-05-02 18:12:51 -0700

    building.java updated

--- UniversitySoftware/src/universitysoftware/Building.java
+++ UniversitySoftware/src/universitysoftware/Building.java
@@ -3,22 +3,23 @@ package universitysoftware;
 import java.util.ArrayList;
 
 public class Building {
-	protected int bldNum;
+	//protected int bldNum;
 	protected String bldName;
 	protected ArrayList<Integer> rmList; //room's room number attribute
 	
 	public Building() {
-		bldNum = 0;
+		//bldNum = 0;
+                bldName = "empty";
 	}
 	
-	public Building(int rn, String name, ArrayList<Integer> rlist) {
-		bldNum = rn;
+	public Building(String name, ArrayList<Integer> rlist) {
+		//bldNum = rn;
 		bldName = name;
 		rmList = rlist;
 	}
 	
-	public void setBldNum(int newBN) {bldNum = newBN;}
-	public int getBldNum() {return bldNum;}
+	//public void setBldNum(int newBN) {bldNum = newBN;}
+	//public int getBldNum() {return bldNum;}
 	
 	public void setBldName(String newName) {bldName = newName;}
 	public String getBldName() {return bldName;}
@@ -33,4 +34,23 @@ public class Building {
 	public void removeRoom(int index) {
             rmList.remove(index);
 	}
+        
+        public void displayRoomList(){
+            if(rmList == null)
+                 System.out.println("empty list");
+            else{
+                System.out.println("Rooms :");   
+                for(Integer room : rmList) 
+                    System.out.print(room+" "); 
+                System.out.println();   
+            }
+        }
+        
+        public int getRmNum(int rm){
+            for (Integer room : rmList) {
+                if (room == rm)
+                    return room;
+            }
+            return 0;
+        }
 }
